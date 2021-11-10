4aa6bfad803ae2e0da86a9fd6e55d866e38312eb
Fozan Alkhalawi <fozanalkhalawi@gmail.com>
2021-11-11 00:55:56 +0300

    implement profile tab bar

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -7,102 +7,129 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         backgroundColor: Colors.white,
-        title: Text('FozanKh', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700)),
-        actions: [
-          SvgPicture.asset(addSvgAsset),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SvgPicture.asset(burgerSvgAsset),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        gradient: LinearGradient(colors: [Color(0xFFF7A34B), Color(0xFFDE0046)]),
-                      ),
-                      padding: EdgeInsets.all(2.5),
-                      child: Container(
-                        height: 90,
-                        width: 90,
-                        padding: EdgeInsets.all(2.5),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: Text('FozanKh', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700)),
+          actions: [
+            SvgPicture.asset(kAddSvgAsset),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SvgPicture.asset(kBurgerSvgAsset),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
                         decoration: BoxDecoration(
-                          gradient: LinearGradient(colors: [Color(0xFFF7A34B), Color(0xFFDE0046)]),
                           shape: BoxShape.circle,
-                          image: DecorationImage(
-                            fit: BoxFit.contain,
-                            image: NetworkImage(
-                              watermelonBGLink,
+                          gradient: LinearGradient(colors: [Color(0xFFF7A34B), Color(0xFFDE0046)]),
+                        ),
+                        padding: EdgeInsets.all(2.5),
+                        child: Container(
+                          height: 90,
+                          width: 90,
+                          padding: EdgeInsets.all(2.5),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [Color(0xFFF7A34B), Color(0xFFDE0046)]),
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              fit: BoxFit.contain,
+                              image: NetworkImage(
+                                kWatermelonBGLink,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Column(
-                      children: [
-                        Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
-                        Text('Posts', style: TextStyle(fontSize: 13)),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
-                        Text('Followers', style: TextStyle(fontSize: 13)),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
-                        Text('Following', style: TextStyle(fontSize: 13)),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Text("Fozan Alkhalawi", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 13)),
-                SizedBox(height: 5),
-                Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt #hashtag",
-                  style: TextStyle(fontSize: 13),
-                ),
-                SizedBox(height: 10),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Text('Edit Profile', style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black)),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 3,
-              children: List.generate(
-                10,
-                (index) => Container(
-                  decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 1)),
-                  child: Image.network(
-                    "https://assets2.razerzone.com/images/pnx.assets/a1f5964573ff5aff38ade71ce3a97d22/go-green-hero-mobile-v2.jpg",
+                      Column(
+                        children: [
+                          Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                          Text('Posts', style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                          Text('Followers', style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('0,000', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                          Text('Following', style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                    ],
                   ),
-                ),
+                  SizedBox(height: 10),
+                  Text("Fozan Alkhalawi", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 13)),
+                  SizedBox(height: 5),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt #hashtag",
+                    style: TextStyle(fontSize: 13),
+                  ),
+                  SizedBox(height: 10),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Text('Edit Profile', style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black)),
+                  ),
+                ],
               ),
             ),
-          )
-        ],
+            TabBar(
+              indicatorColor: Colors.black,
+              tabs: [
+                Tab(icon: SvgPicture.asset(kGridSvgAsset)),
+                Tab(icon: SvgPicture.asset(kMentionsSvgAsset)),
+              ],
+            ),
+            Expanded(
+              flex: 1,
+              child: TabBarView(
+                children: [
+                  PhotosGrid(),
+                  PhotosGrid(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PhotosGrid extends StatelessWidget {
+  const PhotosGrid({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(
+        10,
+        (index) => Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 1)),
+          child: Image.network(
+            "https://assets2.razerzone.com/images/pnx.assets/a1f5964573ff5aff38ade71ce3a97d22/go-green-hero-mobile-v2.jpg",
+          ),
+        ),
       ),
     );
   }
